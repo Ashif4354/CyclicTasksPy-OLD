b3eccdc67f500ec90b4f1fd02d1e6ced16592d1a
Ashif4354 <ashif.abhas@gmail.com>
2024-02-15 08:51:55 +0530

    some changes

--- src/main.py
+++ src/main.py
@@ -2,8 +2,17 @@ from lib import CyclicTasks
 from aiohttp import ClientSession
 from asyncio import run
 from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return jsonify({'message': 'Cyclic Tasks is running...'})
 
 async def main():
     async with ClientSession() as session:
